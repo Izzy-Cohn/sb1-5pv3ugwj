6af470779bd34da51c96ecc319c7b3d3c2d57bf1
Izzy-Cohn <88972127+Izzy-Cohn@users.noreply.github.com>
2024-12-23 16:21:17 -0500

    Update src/components/Logo/index.tsx [BoltSync]

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,16 +1,20 @@
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
 export function Logo() {
   return (
-    <motion.div 
+    <motion.div
       className="flex items-center"
       whileHover={{ scale: 1.05 }}
-      whileTap={{ scale: 0.95 }}>
-      <img
-        src="/logo.svg"
-        alt="News on Shoes Logo"
-        className="h-48 w-48"
-      />
+      whileTap={{ scale: 0.95 }}
+    >
+      <Link to="/">
+        <img
+          src="/logo.svg"
+          alt="News on Shoes Logo"
+          className="h-48 w-48"
+        />
+      </Link>
     </motion.div>
   );
 }
