755c085188b192784a28901274f2099e70c3535a
Izzy-Cohn <88972127+Izzy-Cohn@users.noreply.github.com>
2024-12-23 18:29:48 -0500

    Update src/components/Rankings/rankings.ts [BoltSync]

---
+++ src/components/Rankings/rankings.ts
@@ -0,0 +1,26 @@
+export const rankings = [
+  {
+    title: 'Best Sneakers of 2025 for Flat Feet',
+    slug: 'flat-feet'
+  },
+  {
+    title: 'Best Sneakers of 2025 for Normal to High Arches',
+    slug: 'normal-high-arches'
+  },
+  {
+    title: 'Best Sneakers of 2025 for Knee & Hip Pain',
+    slug: 'knee-and-hip-pain'
+  },
+  {
+    title: 'Best Sneakers of 2025 for Heel Pain',
+    slug: 'heel-pain'
+  },
+  {
+    title: 'Best Sneakers of 2025 for Ball-of-Foot Pain',
+    slug: 'ball-of-foot-pain'
+  },
+  {
+    title: 'Best "No Hands" Sneakers of 2025',
+    slug: 'no-hands'
+  }
+];
