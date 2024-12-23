fc44f2ff257f253c335096f4bd7820d94b5b975a
Izzy-Cohn <88972127+Izzy-Cohn@users.noreply.github.com>
2024-12-23 16:21:20 -0500

    Update src/App.tsx [BoltSync]

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
-import { Hero } from './components/Hero';
-import { FeaturedCategories } from './components/FeaturedCategories';
+import { HomePage } from './pages/HomePage';
+import { CategoryPage } from './pages/CategoryPage';
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
-      <Hero />
-      <FeaturedCategories />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:category" element={<CategoryPage />} />
+      </Routes>
     </div>
   );
 }
