18735d3d4297057f5881cb3edf2f57c063000b71
Izzy Cohn <izzy.cohn@gmail.com>
2025-02-12 16:54:06 -0500

    Fix: make additional changes to vite.config.ts and index.html to resolve URL issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,16 +7,5 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  server: {
-    middleware: [
-      (req, res, next) => {
-        if (req.url.includes('.')) return next();
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(fs.readFileSync('index.html'));
-      },
-    ],
-  },
-  preview: {
-    historyApiFallback: true,
-  },
+  base: '/',
 });
